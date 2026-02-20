9a8a4cc15ead76c67b8243bf6bcedc1be55c6521
Daniel Ocampo <78962214+danielhocampo@users.noreply.github.com>
2026-02-20 13:08:47 +0900

    SQL Problem: Highest Cost Orders
    URL: https://platform.stratascratch.com/coding/9915-highest-cost-orders
    Difficulty:Medium
    ID 9915
    
    Find the customers with the highest daily total order cost between 2019-02-01 and 2019-05-01. If a customer had more than one order on a certain day, sum the order costs on a daily basis. Output each customer's first name, total cost of their items, and the date. If multiple customers tie for the highest daily total on the same date, return all of them.
    
    For simplicity, you can assume that every first name in the dataset is unique.
    
    Tables
    customers
    orders

---
+++ Highest-Cost-Orders.sql
@@ -0,0 +1,16 @@
+with a as
+(
+select distinct c.first_name, o.order_date,
+sum(o.total_order_cost) as total_order_cost,
+dense_rank() over(partition by o.order_date order by sum(o.total_order_cost) desc) as ranks 
+from customers c inner join orders o
+on c.id = o.cust_id
+where order_date between '2019-02-01' and '2019-05-01'
+group by c.first_name, o.order_date
+order by o.order_date, sum(o.total_order_cost) desc
+)
+select first_name, order_date, total_order_cost as max_cost
+from a
+where ranks = 1
+order by order_date, total_order_cost desc
+;

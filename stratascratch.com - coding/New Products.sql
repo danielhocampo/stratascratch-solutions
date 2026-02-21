9745cbe5216ec63440e40aba25ea69d08e7a07db
Daniel Ocampo <78962214+danielhocampo@users.noreply.github.com>
2026-02-21 14:13:22 +0900

    New Products
    Calculate the net change in the number of products launched by companies in 2020 compared to 2019. Your output should include the company names and the net difference.
    (Net difference = Number of products launched in 2020 - The number launched in 2019.)

---
+++ stratascratch.com - coding/New Products.sql
@@ -0,0 +1,11 @@
+with a as
+(
+select company_name,
+count(distinct case when year = 2019 then product_name end) as prod_2019,
+count(distinct case when year = 2020 then product_name end) as prod_2020
+from car_launches
+group by company_name
+)
+select company_name, (prod_2020 - prod_2019) as net_products
+from a
+;
